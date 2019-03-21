d3f9db5f1cd57fbf7673473caf130b79b50da739
DKooijman97 <Kooijman97@gmail.com>
2019-03-21 19:08:45 +0100

    Changed structure of threePointScheme

--- threePointScheme.f90
+++ threePointScheme.f90
@@ -7,8 +7,8 @@ module threePointScheme
    save 
    private 
 	
-   public  threePointSchemeNew, threePointSchemeDelete, initialize_L, Diagonalization, printEigenValuesVectors
-   public  threePointSchemeType, normalize
+   public  threePointSchemeNew, threePointSchemeDelete, Diagonalization, printEigenValuesVectors
+   public  threePointSchemeType
 	
    type threePointSchemeType
       real(KREAL), allocatable 	::  	V(:,:), L(:,:) 
@@ -25,11 +25,15 @@ contains
       type(threePointSchemeType), intent(inout) :: self
    end subroutine 
    
-   !Subroutine initializes L and V, and calculates L according to L = -(1/h²)*S + V
-   subroutine initialize_L(self, Grid)
+! Subroutine which uses Diagonalize to calculate the eigenvalues and eigenvectors 
+! which are used as first trial value in shooting algorithm
+   subroutine Diagonalization(self, Grid) 
       type(threePointSchemeType), intent(inout)  ::  self 
 	  type(gridType), intent(in) :: Grid
-	  integer(KINT)  ::  i 
+	  real(KREAL)  ::  v_int 
+	  integer(KINT):: i
+	  allocate( self%eigenVectors(Grid%N,Grid%N) ) 
+	  allocate( self%eigenValues(Grid%N) ) 
 	  
 	  !Fill in S 
 	  allocate( self%L(Grid%N,Grid%N) ) 
@@ -53,29 +57,8 @@ contains
 	  !Allocate and calculating L 
 	  self%L = (-1.0/(Grid%h**2))*self%L+self%V
 	  
-   end subroutine 
-
-! Subroutine which uses Diagonalize to calculate the eigenvalues and eigenvectors 
-! which are used as first trial value in shooting algorithm
-   subroutine Diagonalization(self, Grid) 
-      type(threePointSchemeType), intent(inout)  ::  self 
-	  type(gridType), intent(in) :: Grid
-	  real(KREAL)  ::  v_int 
-	  allocate( self%eigenVectors(Grid%N,Grid%N) ) 
-	  allocate( self%eigenValues(Grid%N) ) 
-	  
 	  call diagonalize(self%L, self%eigenVectors, eigenvalues = self%eigenValues)
-   end subroutine      
-	
-   subroutine normalize(self, Grid)
-      type(threePointSchemeType), intent(inout)  ::  self 
-	  type(gridType), intent(in) :: Grid
-	  real(KREAL) :: v_int
-	  
-	  call Newton_cotes(self%eigenVectors(:,1),Grid%h,1,size(self%eigenVectors,1),v_int)	
-	  self%eigenValues = self%eigenValues/v_int
-	  print*, "v_int", v_int 
-   end subroutine
+   end subroutine     
 	
    !Prints 1st 10 eigenValues and Vectors
    subroutine printEigenValuesVectors(self, Grid) 	
