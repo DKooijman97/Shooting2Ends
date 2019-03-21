bb996384f5dc6de5495695d1c9cda17955d6f3f0
DKooijman97 <48057587+DKooijman97@users.noreply.github.com>
2019-03-21 18:29:56 +0100

    Changed tab size

--- shooting2EndsMain.f90
+++ shooting2EndsMain.f90
@@ -1,27 +1,27 @@
 program Shooting2EndsTestGridSetup
-		use GridSetup 
-		use UserInput
-		use NumberKinds
-		use threePointScheme
-		use shooting
-		use printModule 
+   use GridSetup 
+   use UserInput
+   use NumberKinds
+   use threePointScheme
+   use shooting
+   use printModule 
+	
+   implicit none 
 		
-		implicit none 
+   type(gridType)  ::  Grid 
+   type(threePointSchemeType)  ::  threePointScheme
+   type(shootingType) ::  Shooting
 		
-		type(gridType)  ::  Grid 
-		type(threePointSchemeType)  ::  threePointScheme
-		type(shootingType) ::  Shooting
-		
-		call readFromFile(Grid)
-		call GridSetupNew(Grid)
-		call createGrid(Grid)
+   call readFromFile(Grid)
+   call GridSetupNew(Grid)
+   call createGrid(Grid)
 				
-		call threePointSchemeNew(threePointScheme)
-		call initialize_L(threePointScheme, Grid)
-		call Diagonalization(threePointScheme, Grid)		
+   call threePointSchemeNew(threePointScheme)
+   call initialize_L(threePointScheme, Grid)
+   call Diagonalization(threePointScheme, Grid)		
 				
-		call shootingNew(Shooting)
-		call calcEigenState(Shooting, Grid, threePointScheme)
-		call printEigenStates(Shooting%y, Grid%MeshPoints, threePointScheme%EigenValues(1), shooting%lambda)
+   call shootingNew(Shooting)
+   call calcEigenState(Shooting, Grid, threePointScheme)
+   call printEigenStates(Shooting%y, Grid%MeshPoints, threePointScheme%EigenValues(1), shooting%lambda)
 end program
 	
