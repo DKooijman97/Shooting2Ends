1d44618c73be2db3bb6a630dff5121294738ecb5
DKooijman97 <Kooijman97@gmail.com>
2019-03-21 20:00:47 +0100

    Output now saved in textfile

--- printModule.f90
+++ printModule.f90
@@ -16,13 +16,19 @@ contains
 	  real(KREAL), intent(in)    :: trialEigenValue
 	  real(KREAL), intent(in)    :: eigenValue
 	  integer(KINT)              :: i 
+	  character(50)              :: fileName
 	  
-	  print'(a30,x,f10.6)', "First trial eigenvalue:", trialEigenValue
-	  print'(a30,x,f10.6)', "The final eigenvalue:", eigenValue
-	  print*, "The normalized eigenvector:"
+	  print*, "Give filename to save data in txt file, in format: filename.txt" 
+	  read*, fileName 
+	  fileName = trim(fileName) 
+	  open(20, FILE = fileName, STATUS = "NEW")
+	  
+	  write(20,'(a30,x,f10.6)')  "First trial eigenvalue:", trialEigenValue
+	  write(20,'(a30,x,f10.6)') "The final eigenvalue:", eigenValue
+	  write(20,*) "The normalized eigenvector:"
 	  
 	  do i = 1, size(vector,1)
-	     print'(f10.6,xx,f10.6)', grid(i), vector(i)
+	     write(20,'(f10.6,x,f10.6)') grid(i), vector(i)
 	  end do
    end subroutine 
 end module 	  
