cc74367196974134f259db1e3cde474b0362912a
DKooijman97 <Kooijman97@gmail.com>
2019-03-28 17:44:31 +0100

    Multiple energy levels!

--- printModule.f90
+++ printModule.f90
@@ -12,8 +12,8 @@ module printModule
    
 contains 
    subroutine printEigenStates(vector, grid, trialEigenValue, eigenValue)
-      real(KREAL), intent(in)    :: vector(:), grid(:)
-	  real(KREAL), intent(in)    :: trialEigenValue
+      real(KREAL), intent(in)    :: vector(:,:), grid(:)
+	  real(KREAL), intent(in)    :: trialEigenValue(:)
 	  real(KREAL), intent(in)    :: eigenValue
 	  integer(KINT)              :: i 
 	  character(50)              :: fileName
@@ -23,13 +23,15 @@ contains
 	  fileName = trim(fileName) 
 	  open(20, FILE = fileName, STATUS = "NEW")
 	  
-	  write(20,'(a30,x,f10.6)')  "First trial eigenvalue:", trialEigenValue
-	  write(20,'(a30,x,f10.6)') "The final eigenvalue:", eigenValue
-	  write(20,*) "The normalized eigenvector:"
+	  !write(20,'(a30,x,f10.6)')  "First trial eigenvalue:", trialEigenValue
+	  !write(20,'(a30,x,f10.6)') "The final eigenvalue:", eigenValue
+	  !write(20,*) "The normalized eigenvectors:"
 	  
-	  do i = 1, size(vector,1)
-	     write(20,'(f10.6,x,f10.6)') grid(i), vector(i)
-	  end do
+	  do i = 1, size(grid,1)
+	     write(20,*) grid(i), vector(i,:)
+	  enddo
+	 
+	  
    end subroutine 
 end module 	  
    
--- shooting_2.f90
+++ shooting_2.f90
@@ -11,10 +11,10 @@ module shooting
    private 
    
    public shootingType
-   public shootingNew, shootingDelete, calcEigenState
+   public shootingNew, shootingDelete, energyStates 
    
    type shootingType 
-      real(KREAL), allocatable  :: y(:) 
+      real(KREAL), allocatable  :: y(:,:) 
       real(KREAL), allocatable  :: yIn(:)    
       real(KREAL), allocatable  :: yOut(:)
       real(KREAL)               :: lambda  
@@ -32,49 +32,71 @@ contains
       type(shootingType), intent(inout)  :: self
    end subroutine
    
-   subroutine calcEigenState(self, Grid, threePoint)
+   
+   subroutine energyStates(self, Grid, threePoint) 
       type(shootingType), intent(inout)      :: self
-      type(GridType), intent(in)             :: Grid
-      type(threePointSchemeType), intent(in) :: ThreePoint
-      real(KREAL)                            :: minDifference, difference
-      real(KREAL)                            :: Intergral  
-      integer(KINT)                          :: i=0
+	  type(GridType), intent(in)             :: Grid
+      type(ThreePointSchemeType), intent(in) :: threePoint
+	  real(KREAL)                            :: firstLambda
+	  integer(KINT)                          :: i
 	  
-      self%x_m = Grid%N/2
-      print*, "xm", self%x_m
-      !Initialize lambda with first threePointScheme
-      self%lambda = threePoint%eigenValues(1)
+	  !Initialize lambda with threePointScheme
       
-      !Set min difference and initialize difference 
-      print*, "Set min difference"
-      read(*,*) minDifference
-      difference = 10000
-	  
-      allocate( self%yOut(self%x_m+1) )
+      
+	  !Calculate x_m 
+	  self%x_m = Grid%N/2
+      
+	  !Allocate needed vectors
+	  allocate( self%y(Grid%N,10) )
+	  allocate( self%yOut(self%x_m+1) )
       allocate( self%yIn(Grid%N) )
+	  
+	  self%x_m = Grid%N/2
+      print*, "xm", self%x_m
       self%yOut(:2) = 1d-3
       self%yIn(Grid%N-1:) = 1d-3
-
-      !Loop which tests different lambdas until difference is small enough 
+   
+      do i = 1, 10 
+         firstLambda  = ThreePoint%eigenValues(i)*(-1)
+		 call calcEigenState( self,Grid,firstLambda, self%y(:,i) )
+	  enddo
+   
+   end subroutine
+   
+   
+   subroutine calcEigenState(self, Grid, firstLambda, y)
+      type(shootingType), intent(inout)      :: self
+      type(GridType), intent(in)             :: Grid
+      real(KREAL)                            :: Intergral 
+	  real(KREAL), intent(in)                :: firstLambda
+      real(KREAL)                            :: minDifference, difference
+	  real(KREAL), intent(inout)             :: y(:) 
+	  integer(KINT)                          :: i=0
+	  
+      self%Lambda = firstLambda
+	  
+	  !Set min difference and initialize difference 
+      print*, "Set min difference"
+      minDifference = 1d-10
+      difference = 10000
+	  
+	  !Loop which tests different lambdas until difference is small enough 
       do while (difference>minDifference)
          i = i + 1
          call InOut(self%yOut, 3, self%x_m, Grid%h, self%lambda,Grid%V, 0)
          call InOut(self%yIn, Grid%N-2, self%x_m, Grid%h, self%lambda,Grid%V, 1)  
 		 
-		
          call calcDLambda(self, Grid) 
-	     !1print*, "dLambda",self%dLambda
          self%Lambda = self%Lambda - self%dLambda
 		 
          difference = abs(self%dLambda)
       enddo 
 	   
-      !Combine the two 
-      allocate( self%y(grid%N) ) 
-      self%y(:2) = 1d-3
-      self%y(Grid%N-1:) = 1d-3
-	  call InOut(self%y, 3, Grid%N-3, Grid%h, self%lambda,Grid%V, 0)
-      self%y = self%y-self%y(1)	   
+      !Combine the inwards and outwards: 
+      y(:2) = 1d-3
+      y(Grid%N-1:) = 1d-3
+	  call InOut(y, 3, Grid%N-3, Grid%h, self%lambda,Grid%V, 0)
+      y = y-y(1)
    end subroutine 
    
    subroutine InOut(y, startPoint, x_m, h, lambda,V, IN_OUT)
@@ -84,7 +106,7 @@ contains
       real(KREAL), intent(in)   :: V(:,:)                    !Rank 2 matrix, with potential on the diagonal 
       integer(KINT), intent(in) :: startPoint, x_m           !Start- end matching point of calculation
       integer(KINT), intent(in) :: IN_OUT                    !Outwards: IN = 0, Inwards: IN = 1; default = Outwards
-       
+
       real(KREAL)   :: intergral = 0 
       integer(KINT) :: i 
 	   
@@ -95,8 +117,7 @@ contains
          enddo 
 	      
          !Normalize
-         	
-		 call Newton_cotes(y,h,1,size(y,1),intergral)
+		 call Newton_cotes(y**2,h,1,size(y,1),intergral)
 	     y = y/intergral
       case (1) 
          do i = startPoint+1, x_m-1, -1      
@@ -104,7 +125,7 @@ contains
          enddo 
 	      
          !Normalize
-	     call Newton_cotes(y,h,1,size(y,1),intergral)
+	     call Newton_cotes(y**2,h,1,size(y,1),intergral)
 	     y = y/intergral 		  
       end select 
    end subroutine
@@ -117,7 +138,7 @@ contains
 	   
       derivativeOut = ( self%yOut(self%x_m+1)-self%yOut(self%x_m-1) ) / 2*Grid%h
       derivativeIn  = ( self%yIn(self%x_m+1)-self%yIn(self%x_m-1) ) / 2*Grid%h
-	   
+	  
       call Newton_cotes(self%yOut**2,Grid%h,1,self%x_m,intergralOut2)
       call Newton_cotes(self%yIn**2,Grid%h,self%x_m,Grid%N,intergralIn2)
 	   
