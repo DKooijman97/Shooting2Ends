452420b8e8bbdfcc75ffb4dc3221ce5a7a45eef5
DKooijman97 <Kooijman97@gmail.com>
2019-03-30 13:29:39 +0100

    added getters to shooting & 3point

--- shooting2EndsMain.f90
+++ shooting2EndsMain.f90
@@ -7,33 +7,40 @@ program Shooting2EndsTestGridSetup
    use printModule 
 	
    implicit none 
-		
+   
+   !Define needed types:
    type(gridType)              ::  Grid, gridShooting
    type(threePointSchemeType)  ::  threePointScheme
    type(shootingType)          ::  Shooting
 	
+   !Define needed variables: 	
+   real(KREAL), allocatable    ::  trialEigenValues(:)	               !The trial eigenvalues for shooting algorithm, calculated with the three point scheme method
+   real(KREAL), allocatable    ::  eigenVectors(:,:), eigenValues(:)   !Final eigenvectors and eigenvalues as calculated with the shooting algorithm 	
+   
    call readFromFile(Grid,Shooting)
    
    call GridSetupNew(Grid)
    call createGrid(Grid)
 				
    call threePointSchemeNew(threePointScheme)
-   call Diagonalization(threePointScheme, Grid)		
+   call Diagonalization(threePointScheme, Grid)	
+   call getTrialEigenValues(threePointScheme,trialEigenValues)
   
    !Create New grid, with 500 points 
-   gridShooting%N             = 500
+   gridShooting%N             = 280
    gridShooting%startInterval = Grid%startInterval
    gridShooting%endInterval   = Grid%endInterval
    gridShooting%Potential     = Grid%potential
    call GridSetupNew(GridShooting)
    call createGrid(GridShooting)
-	  
+   
   
    call shootingNew(Shooting)
-   call energyStates(Shooting, GridShooting, threePointScheme)
-  
-   call printEigenStates(Shooting%y, GridShooting%MeshPoints,GridShooting%V, Shooting%firstLambda &
-                        ,shooting%LambdaVector,shooting%energyLevels)
+   call energyStates(Shooting, GridShooting, trialEigenValues)
+   call getEigenStates(Shooting, eigenVectors, eigenValues) 
+   
+   call printEigenStates(eigenVectors, GridShooting%MeshPoints,GridShooting%V, trialEigenValues &
+                        ,eigenValues,shooting%energyLevels)
 
 end program
 	
